c667825de900d7e03f11f4d65be9cd04eea4edf6
guichristmann <guichristmann@gmail.com>
2018-11-24 00:35:05 +0000

    uhmmm

--- scripts/inno_arm_control.py
+++ scripts/inno_arm_control.py
@@ -9,6 +9,50 @@ from inno_arm_control_msgs.msg import *
 
 from kinematics import *
 
+
+theta_0_max = 2550 # equivale a 90 graus. 
+theta_0_min =  750 # equivale a -90 graus 
+theta_0_med = int((theta_0_max+theta_0_min)/2.)
+
+theta_1_max = 1250 # equivale a 90 graus. 
+theta_1_min =  500 # equivale a 0 graus 
+theta_1_med = int((theta_1_max+theta_1_min)/2.)
+
+theta_2_min = 1100 # equivale a 0 graus
+theta_2_max = 2100 # equivale a 90 graus.
+theta_2_med = int((theta_2_max+theta_2_min)/2.)
+
+theta_3_max = 2500 # equivale a 90 graus.
+theta_3_min = 800 # equivale a -90 graus
+theta_3_med = int((theta_3_max+theta_3_min)/2.)
+
+theta_4_max = 2400 # equivale a 90 graus.
+theta_4_min = 500 # equivale a -90 graus
+theta_4_med = int((theta_4_max+theta_4_min)/2.)
+
+theta_5_max = 2400 # equivale a 90 graus
+theta_5_min = 500 # equivale a -90 graus
+theta_5_med = int((theta_5_max+theta_5_min)/2.)
+
+theta_end_max = 2800
+theta_end_min = 2200
+
+theta_max = [[theta_0_max, math.pi/2], [theta_1_max, math.pi/2], [theta_2_max, math.pi/2], [theta_3_max, math.pi/2], [theta_4_max, math.pi/2], [theta_5_max, math.pi/2], [theta_end_max, math.pi/2]]
+theta_min = [[theta_0_min, -math.pi/2], [theta_1_min, 0.0], [theta_2_min, 0], [theta_3_min, -math.pi/2], [theta_4_min, -math.pi/2], [theta_5_min, -math.pi/2], [theta_end_min, 0.0]]
+
+def norm_angles(angles):	
+	raw_pose = []
+
+	for i in range(0, angles.shape[0]):
+		raw_pose.append((float(float(angles[i]) - theta_min[i][1])/float(theta_max[i][1] - theta_min[i][1]))*(theta_max[i][0]-theta_min[i][0]) + theta_min[i][0])
+
+	print(raw_pose[1])
+        print(raw_pose[2])
+        raw_pose[2] = raw_pose[2] - (raw_pose[1]-theta_min[1][0])
+        print(raw_pose[2])
+
+	return raw_pose
+
 def inno_arm_control():
 
 	inno_arm_control_pub = rospy.Publisher("/set_joint_angles", JointMsg, queue_size=10)
@@ -31,7 +75,9 @@ def inno_arm_control():
 	
 			angles += delta_angles
 
-			inno_arm_control_pub.publish([angles[0], angles[1], angles[2], angles[3], angles[4], angles[5]])
+			raw_angles = norm_angles(angles)
+
+			inno_arm_control_pub.publish([raw_angles[0], raw_angles[1], raw_angles[2], raw_angles[3], raw_angles[4], raw_angles[5]])
 
 			print(angles)
 	
--- scripts/set_joint_angles.py
+++ scripts/set_joint_angles.py
@@ -9,64 +9,17 @@ import rospy
 from std_msgs.msg import *
 from inno_arm_control_msgs.msg import *
 
-
 serialComm = serial.Serial()
 serialComm.port = '/dev/ttyUSB0'
 serialComm.baudrate = 9600
 
-
-theta_0_max = 2550 # equivale a 90 graus. 
-theta_0_min =  750 # equivale a -90 graus 
-theta_0_med = int((theta_0_max+theta_0_min)/2.)
-
-theta_1_max = 1250 # equivale a 90 graus. 
-theta_1_min =  500 # equivale a 0 graus 
-theta_1_med = int((theta_1_max+theta_1_min)/2.)
-
-theta_2_min = 1100 # equivale a 0 graus
-theta_2_max = 2100 # equivale a 90 graus.
-theta_2_med = int((theta_2_max+theta_2_min)/2.)
-
-theta_3_max = 2500 # equivale a 90 graus.
-theta_3_min = 800 # equivale a -90 graus
-theta_3_med = int((theta_3_max+theta_3_min)/2.)
-
-theta_4_max = 2400 # equivale a 90 graus.
-theta_4_min = 500 # equivale a -90 graus
-theta_4_med = int((theta_4_max+theta_4_min)/2.)
-
-theta_5_max = 2400 # equivale a 90 graus
-theta_5_min = 500 # equivale a -90 graus
-theta_5_med = int((theta_5_max+theta_5_min)/2.)
-
-theta_end_max = 2800
-theta_end_min = 2200
-
-theta_max = [[theta_0_max, math.pi/2], [theta_1_max, math.pi/2], [theta_2_max, math.pi/2], [theta_3_max, math.pi/2], [theta_4_max, math.pi/2], [theta_5_max, math.pi/2], [theta_end_max, math.pi/2]]
-theta_min = [[theta_0_min, -math.pi/2], [theta_1_min, 0.0], [theta_2_min, 0], [theta_3_min, -math.pi/2], [theta_4_min, -math.pi/2], [theta_5_min, -math.pi/2], [theta_end_min, 0.0]]
-
-def norm_angles(data):
-	raw_pose = []
-
-	for i in range(0, len(data.angle)):
-		raw_pose.append((float(float(data.angle[i]) - theta_min[i][1])/float(theta_max[i][1] - theta_min[i][1]))*(theta_max[i][0]-theta_min[i][0]) + theta_min[i][0])
-
-	print(raw_pose[1])
-        print(raw_pose[2])
-        raw_pose[2] = raw_pose[2] - (raw_pose[1]-theta_min[1][0])
-        print(raw_pose[2])
-
-	return raw_pose
-
-def callback(data):
-
-	raw_angles = norm_angles(data)
+def callback(data):	
 
 	rospy.loginfo(data)
 
 	k = 1
 	while k < 7:
-		msg = "#"+str(k)+"P" + str(int(raw_angles[k-1])) + "T100\r\n"
+		msg = "#"+str(k)+"P" + str(int(data.angle[k-1])) + "T100\r\n"
 		#print(msg)
 		k += 1
 		
@@ -78,9 +31,7 @@ def set_joints_angles():
 
     rospy.Subscriber("/set_joint_angles", JointMsg, callback)
 
-    rospy.spin()
-    
-    
+    rospy.spin()    
 
 if __name__ == "__main__":
     try:
